b6915964f3d6cdd0c3a7c84a44199ef7a3e021b3
Lukasz Kurylo <lkurylo@2kmm.pl>
2021-06-12 00:43:11 +0200

    first simple passing unit test

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -6,9 +6,9 @@ namespace tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void simple_passing_test()
         {
-
+            Assert.Equal(1, 1);
         }
     }
 }
